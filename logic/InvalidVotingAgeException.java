4ebb30de65c0cf58256e87daab7c5d9d8ca7a17d
matusmatok <80100020+matusmatok@users.noreply.github.com>
2024-05-02 20:55:16 +0200

    Add javadoc

--- logic/InvalidVotingAgeException.java
+++ logic/InvalidVotingAgeException.java
@@ -3,10 +3,8 @@ package logic;
 /**
  * @author Filip Hromada
  * @version 1.0
- * @param message - správa výnimky
  * Definicia vlastnej výnimky InvalidVotingAgeException dediacej od Exception s konštruktorom s parametrom message
  */
-
 public class InvalidVotingAgeException extends Exception {
     public InvalidVotingAgeException(String message) {
         super(message);
